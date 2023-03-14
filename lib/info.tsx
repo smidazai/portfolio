24b5bb830e1ba07668751185bba28447baacf47b
Ghislain Durif <gd.dev@libertymail.net>
2023-03-14 17:10:36 +0100

    fix some typos + switch to decreasing chronological order for publications and talks

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,14 +5,14 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      <p>ATER (Attachée Temporaire d'Enseignement et de la Recherche) à TSE (Toulouse School of Economics), Université de Toulouse Capitole, Toulouse, France.</p>
+      <p>ATER (Attachée Temporaire d'Enseignement et de Recherche) à TSE (Toulouse School of Economics), Université de Toulouse Capitole, Toulouse, France.</p>
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-    <b>Thème de recherche : </b><p>Tests de comparaison de deux populations, Tests paramétriques et non paramétriques, données fonctionnelles, statistique de scan, statistique spatiale, détection d'agrégats, théorèmes asymptotiques, données multivariées. 
+    <b>Thème de recherche : </b><p>Tests de comparaison de deux populations, Tests paramétriques et non paramétriques, données fonctionnelles, statistiques de scan, statistiques spatiales, détection d'agrégats, théorèmes asymptotiques, données multivariées.
     </p>
     </>
   );
--- pages/recherche.tsx
+++ pages/recherche.tsx
@@ -5,22 +5,7 @@ export default function RecherchePage() {
     <Fragment>
     <section>
       <h1 className="font-bold text-3xl font-serif">Recherche</h1>
-      <a className="my-5 max-w-sm w-full lg:max-w-full lg:flex" href="https://www.sciencedirect.com/science/article/abs/pii/S0167947321002127" target="_blank">
-        <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url("https://ars.els-cdn.com/content/image/1-s2.0-S0167947321X00114-cov200h.gif")`, backgroundSize: "auto" }}>
-        </div>
-        <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
-          <div className="mb-8">
-            <div className="text-gray-900 font-bold text-xl mb-2">A Wilcoxon-Mann-Whitney spatial scan statistic for functional data.</div>
-            <p className="text-gray-700 text-base">Computational Statistics and Data Analysis.</p>
-          </div>
-          <div className="flex items-center">
-            <div className="text-sm">
-              <p className="text-gray-900 leading-none">Smida, Z., Cucala, L., Gannoun, A. and Durif, G. (2021)</p>
-            </div>
-          </div>
-        </div>
-      </a>
-      
+
       <a className="my-5 max-w-sm w-full lg:max-w-full lg:flex" href="https://www.tandfonline.com/doi/abs/10.1080/10485252.2022.2064997?journalCode=gnst20" target="_blank">
         <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url("https://www.tandfonline.com/action/showCoverImage?doi=10.1080/gnst20.v034.i02")`, backgroundSize: "auto" }}>
         </div>
@@ -52,10 +37,28 @@ export default function RecherchePage() {
         </div>
       </div>
       </a>
+
+      <a className="my-5 max-w-sm w-full lg:max-w-full lg:flex" href="https://www.sciencedirect.com/science/article/abs/pii/S0167947321002127" target="_blank">
+        <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url("https://ars.els-cdn.com/content/image/1-s2.0-S0167947321X00114-cov200h.gif")`, backgroundSize: "auto" }}>
+        </div>
+        <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-xl mb-2">A Wilcoxon-Mann-Whitney spatial scan statistic for functional data.</div>
+            <p className="text-gray-700 text-base">Computational Statistics and Data Analysis.</p>
+          </div>
+          <div className="flex items-center">
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none">Smida, Z., Cucala, L., Gannoun, A. and Durif, G. (2021)</p>
+            </div>
+          </div>
+        </div>
+      </a>
+
     </section>
     <section>
     <h1 className="font-bold text-3xl font-serif my-5">Prépublications</h1>
     <div className="my-5 max-w-sm w-full lg:max-w-full lg:flex">
+
         <a className="border-r border-b border-l border-t border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full" href="https://gdurif.pages.math.cnrs.fr/funStatTest/" target="_blank">
           <div className="mb-8">
             <div className="text-gray-900 font-bold text-xl mb-2">funStatTest: Statistical Testing For Functional Data.</div>
@@ -67,6 +70,7 @@ export default function RecherchePage() {
             </div>
           </div>
         </a>
+
       </div>
     </section>
     <section>
@@ -77,142 +81,10 @@ export default function RecherchePage() {
           <li className="mb-2">
             <div className="flex items-center mb-1">
               <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Mars 2019 - Participation aux journées de doctorantes en probabilités</div>
+              <div className="flex-1 ml-4 font-medium">Fév. 2023 - Séminaire de Probabilités-Statistique</div>
             </div>
             <div className="ml-12">
-              Université de Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Mars 2019 - Participation aux journées de doctorantes en probabilités</div>
-            </div>
-            <div className="ml-12">
-              Université de Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Juin 2019 - Participation aux  51èmes Journées de Statistique de la Société Française de Statistiques</div>
-            </div>
-            <div className="ml-12">
-              Université de Lorraine, Nancy.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Juin 2020 - Communication acceptée aux 52èmes Journées de Statistique de la Société Française de Statistiques (reportées en 2021)</div>
-            </div>
-            <div className="ml-12">
-              Nice.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Mai 2021 - Séminaire Co-organisé par le laboratoire de Mathématiques de l’Université d’Avignon et l’Unité BioSP d’INRAE d’Avignon.</div>
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Juin 2021 - Exposé aux 52èmes Journées de Statistique de la Société Française de Statistique</div>
-            </div>
-            <div className="ml-12">
-              Nice.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Oct. 2021 - Séminaire de Probabilités et Statistique, Institut Montpelliérain Alexender Grothendieck</div>
-            </div>
-            <div className="ml-12">
-              Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Nov. 2021 - Séminaire de doctorant.e.s de Montpellier</div>
-            </div>
-            <div className="ml-12">
-              Institut Montpelliérain Alexender Grothendieck, Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Déc. 2021 - Exposé au Forum des Jeunes Mathématicien-ne-s</div>
-            </div>
-            <div className="ml-12">
-              Besançon.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Fév. 2022 - 10ème Journée YSP: Young Probabilists and Statisticians, IHP</div>
-            </div>
-            <div className="ml-12">
-              Paris.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Mars 2022 - Séminaire de doctorant.e.s de Montpellier</div>
-            </div>
-            <div className="ml-12">
-              Institut Montpelliérain Alexender Grothendieck, Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Avr. 2022 - Exposé aux 9èmes Rencontres des Jeunes Statisticien-ne-s</div>
-            </div>
-            <div className="ml-12">
-              Porquerolles.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Mai 2022 - Exposé aux 20th International Workshop in Spatial Econometrics and Statistics</div>
-            </div>
-            <div className="ml-12">
-              Lille.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Juin 2022 - Poster aux 2èmes Journées IMAG-IMT</div>
-            </div>
-            <div className="ml-12">
-              Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Juin 2022 - Exposé au Journée des doctorants à l’IMAG</div>
-            </div>
-            <div className="ml-12">
-              Montpellier.
-            </div>
-          </li>
-          <li className="mb-2">
-            <div className="flex items-center mb-1">
-              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Août 2022 - Exposé au 24th International Conference on Computational Statistics</div>
-            </div>
-            <div className="ml-12">
-              Bologna, Italy.
+              Laboratoire de mathématiques de Besançon, Besançon.
             </div>
           </li>
           <li className="mb-2">
@@ -227,10 +99,133 @@ export default function RecherchePage() {
           <li className="mb-2">
             <div className="flex items-center mb-1">
               <div className="bg-gray-500 rounded-full h-8 w-8"></div>
-              <div className="flex-1 ml-4 font-medium">Fév. 2023 - Séminaire de Probabilités-Statistique</div>
+              <div className="flex-1 ml-4 font-medium">Août 2022 - Exposé à la 24th International Conference on Computational Statistics</div>
             </div>
             <div className="ml-12">
-              Laboratoire de mathématiques de Besançon, Besançon.
+              Bologna, Italy.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Juin 2022 - Exposé à la Journée des doctorants à l’IMAG</div>
+            </div>
+            <div className="ml-12">
+              Montpellier.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Juin 2022 - Poster aux 2èmes Journées IMAG-IMT</div>
+            </div>
+            <div className="ml-12">
+              Montpellier.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Mai 2022 - Exposé au 20th International Workshop in Spatial Econometrics and Statistics</div>
+            </div>
+            <div className="ml-12">
+              Lille.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Avr. 2022 - Exposé aux 9èmes Rencontres des Jeunes Statisticien-ne-s</div>
+            </div>
+            <div className="ml-12">
+              Porquerolles.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Mars 2022 - Séminaire des doctorant.e.s de Montpellier</div>
+            </div>
+            <div className="ml-12">
+              Institut Montpelliérain Alexender Grothendieck, Montpellier.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Fév. 2022 - 10ème Journée YSP: Young Probabilists and Statisticians, IHP</div>
+            </div>
+            <div className="ml-12">
+              Paris.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Déc. 2021 - Exposé au Forum des Jeunes Mathématicien-ne-s</div>
+            </div>
+            <div className="ml-12">
+              Besançon.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Nov. 2021 - Séminaire des doctorant.e.s de Montpellier</div>
+            </div>
+            <div className="ml-12">
+              Institut Montpelliérain Alexender Grothendieck, Montpellier.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Oct. 2021 - Séminaire de Probabilités et Statistique, Institut Montpelliérain Alexender Grothendieck</div>
+            </div>
+            <div className="ml-12">
+              Montpellier.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Juin 2021 - Exposé aux 52èmes Journées de Statistique de la Société Française de Statistique</div>
+            </div>
+            <div className="ml-12">
+              Nice.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Mai 2021 - Séminaire co-organisé par le laboratoire de Mathématiques de l’Université d’Avignon et l’Unité BioSP d’INRAE d’Avignon.</div>
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Juin 2020 - Communication acceptée aux 52èmes Journées de Statistique de la Société Française de Statistiques (reportées en 2021)</div>
+            </div>
+            <div className="ml-12">
+              Nice.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Juin 2019 - Participation aux 51èmes Journées de Statistique de la Société Française de Statistiques</div>
+            </div>
+            <div className="ml-12">
+              Université de Lorraine, Nancy.
+            </div>
+          </li>
+          <li className="mb-2">
+            <div className="flex items-center mb-1">
+              <div className="bg-gray-500 rounded-full h-8 w-8"></div>
+              <div className="flex-1 ml-4 font-medium">Mars 2019 - Participation aux journées des doctorantes en probabilités</div>
+            </div>
+            <div className="ml-12">
+              Université de Montpellier.
             </div>
           </li>
         </ul>
